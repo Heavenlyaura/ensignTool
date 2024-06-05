e4055645e686a7b1d38639e40a1e0dbccb9eb620
heavenlyaura <goodnessokafor462@gmail.com>
2024-06-05 04:18:08 +0100

    "Updated JSON data, added new HTML elements and styles, and modified JavaScript code for graduation review functionality."

--- script/gradReview.js
+++ script/gradReview.js
@@ -2,16 +2,61 @@ document.addEventListener('DOMContentLoaded', () => {
   const uncompletedDiv = document.querySelector('.uncompleted');
   const storedCourses = localStorage.getItem('notCompleted')
   const uncompletedCourses = JSON.parse(storedCourses)
+  const gradTotal = document.querySelector('.grad-total')
+  const completeAnalysisDiv = document.querySelector('.completeAnalysis')
+  const finish = document.querySelector('#finish')
+  const creditSavedh3 = document.querySelector('.credit-saved')
+  const oldDegree = 120
+  let totalCredits = parseInt(localStorage.getItem('totalCredit'))
+  completeAnalysisDiv.style.display = 'none'
+
   if (storedCourses) {
-    let list = document.createElement('ul')
+
+    let uncompleted = `
+    <div class="uncompleteHead"> 
+      <h4>Course Code</h4>
+      <h4>Course Title</h4>
+      <h4>Credits</h4>
+      <h4 class="added">Add</h4>
+    </div>
+    <div>
+    `
     uncompletedCourses.forEach(course => {
-      let listItem = document.createElement('li')
-      let listText = `${course.courseCode}: ${course.courseTitle} - ${course.creditCell} - ${course.caption}`
-      listItem.textContent = listText
-      list.appendChild(listItem)
+      console.log(course.creditCell)
+      uncompleted +=
+        `<span class="uncompleteBody">
+      <p>${course.courseCode}</p>
+      <p>${course.courseTitle}</p>
+      <p>${course.creditCell}</p>
+      <button class="addGrad" data-course="${course.courseCode}" data-credits="${course.creditCell}">Add</button>
+      </span>
+      `
     });
-    uncompletedDiv.appendChild(list)
+    uncompleted += `</div>`
+    // console.log(uncompleted)
+    uncompletedDiv.innerHTML = uncompleted
   }
+
+  const addButton = document.querySelectorAll('.addGrad')
+  addButton.forEach(button => {
+    button.addEventListener('click', () => {
+      const courseCredits = button.dataset.credits
+      totalCredits += parseInt(courseCredits)
+      const parentRow = button.parentElement
+      gradTotal.textContent = `Total: ${totalCredits}`
+      parentRow.remove()
+    })
+  });
+
+
+  finish.addEventListener('click', () => {
+    completeAnalysisDiv.style.display = 'block'
+    let creditSaved = oldDegree - totalCredits
+    creditSavedh3.textContent = creditSaved
+
+  })
+
+
 
 
 })
